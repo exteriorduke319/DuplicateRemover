5d0f8254fbf448cd7300ae2130c8dbc5eb19cf84
exteriorduke319 <yarnevictanner@gmail.com>
2023-11-10 09:33:31 -0500

    Init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        String wordInput;
+        String wordOutput = "";
+
+        System.out.println("Enter a word: ");
+        wordInput = scan.nextLine();
+
+        for (int i = 0; i < wordInput.length(); i++){
+            if(!wordOutput.contains(wordInput.substring(i, i+1))) {
+                wordOutput += wordInput.substring(i,i+1);
+            }
+        }
+
+        System.out.println(wordOutput);
     }
 }
